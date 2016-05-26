9f1d5f9ad102daa2b9778632440a6a28ad4bd095
Mr.熊 <Mr.@DESKTOP-38TVFU1>
2016-05-26 21:23:54 +0800

    5-26

--- BBTAdroid/src/com/bbt/main/ui/OrderdetailsActivity.java
+++ BBTAdroid/src/com/bbt/main/ui/OrderdetailsActivity.java
@@ -1,5 +1,7 @@
 package com.bbt.main.ui;
 
+import com.bbt.main.tool.ActionBarBuilder;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,5 +12,8 @@ protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.order_details);
 	System.out.println("details");
+	new ActionBarBuilder(this).setTitleText("订单详情");
+	
+	
 }
 }
